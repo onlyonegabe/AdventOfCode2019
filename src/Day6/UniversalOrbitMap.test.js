bd3c859b25e3d0e5ed5414aa0636cb77c44062dd
Gabriel Ortiz <gabriel.j.ortiz@gmail.com>
2020-02-22 14:07:14 -0800

    run all tests

--- src/Day6/UniversalOrbitMap.test.js
+++ src/Day6/UniversalOrbitMap.test.js
@@ -1,29 +1,4 @@
-function createItems(input) {
-    let items = {}
-    input.split('\n').forEach(element => {
-        let parsedItem = element.split(')')
-        items[parsedItem[1]] = parsedItem[0]
-    })
-    return items
-}
-
-function findIndirectOrbits(items) {
-    const entries = Object.entries(items)
-    let indirectOrbits = 0
-    for (const [orbitingObject, object] of entries) {
-        if (object === "COM") {
-            continue;
-        }
-
-        let something = object
-        while(something !== "COM") {
-            let object = items[something]
-            something = object
-            indirectOrbits++
-        }
-    }
-    return indirectOrbits
-}
+import {createItems, findIndirectOrbits} from './UniversalOrbitMap'
 
 test("Creates orbit item", () => {
     const input = "COM)NQY"
