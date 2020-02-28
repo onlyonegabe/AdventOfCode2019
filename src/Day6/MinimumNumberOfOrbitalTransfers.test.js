cb766e483f24fc2767ffe07842e8e8edbe13b957
Gabriel Ortiz <gabriel.j.ortiz@gmail.com>
2020-02-27 17:30:38 -0800

    moving functions into it's own file

--- src/Day6/MinimumNumberOfOrbitalTransfers.test.js
+++ src/Day6/MinimumNumberOfOrbitalTransfers.test.js
@@ -1,39 +1,4 @@
-import {createItems} from './UniversalOrbitMap'
-
-function getOrbitalPath(items, startingPoint) {
-    let orbitalPath = []
-    let object = startingPoint
-
-    while(object !== "COM") {
-        object = items[object]
-        orbitalPath.push(object)
-    }
-    return orbitalPath
-}
-
-function getOrbitalTransfers(myPath, santaPath) {
-    let orbitalTransfers = 0
-    for (let i = 0; i < myPath.length; i++) {
-        if (santaPath.find(element => element === myPath[i])) {
-            break
-        } 
-        orbitalTransfers++
-    }
-    for (let i = 0; i < santaPath.length; i++) {
-        if (myPath.find(element => element === santaPath[i])) {
-            break
-        } 
-        orbitalTransfers++
-    }
-    return orbitalTransfers
-}
-
-function findMinimumOrbitalTransfers(items) {
-    let myPath = getOrbitalPath(items, 'YOU')
-    let santaPath = getOrbitalPath(items, 'SAN')
-    let orbitalTransfers = getOrbitalTransfers(myPath, santaPath)
-    return orbitalTransfers
-}
+import {createItems, findMinimumOrbitalTransfers} from './UniversalOrbitMap'
 
 test('minimum number of orbital transfers sample example', () => {
     let fs = require('fs')
--- src/Day6/UniversalOrbitMap.js
+++ src/Day6/UniversalOrbitMap.js
@@ -22,3 +22,38 @@ export function findIndirectOrbits(items) {
     }
     return indirectOrbits
 }
+
+export function findMinimumOrbitalTransfers(items) {
+    function getOrbitalPath(items, startingPoint) {
+        let orbitalPath = []
+        let object = startingPoint
+    
+        while(object !== "COM") {
+            object = items[object]
+            orbitalPath.push(object)
+        }
+        return orbitalPath
+    }
+    
+    function getOrbitalTransfers(myPath, santaPath) {
+        let orbitalTransfers = 0
+        for (let i = 0; i < myPath.length; i++) {
+            if (santaPath.find(element => element === myPath[i])) {
+                break
+            } 
+            orbitalTransfers++
+        }
+        for (let i = 0; i < santaPath.length; i++) {
+            if (myPath.find(element => element === santaPath[i])) {
+                break
+            } 
+            orbitalTransfers++
+        }
+        return orbitalTransfers
+    }
+    
+    let myPath = getOrbitalPath(items, 'YOU')
+    let santaPath = getOrbitalPath(items, 'SAN')
+    let orbitalTransfers = getOrbitalTransfers(myPath, santaPath)
+    return orbitalTransfers
+}
